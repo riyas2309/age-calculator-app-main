4aa4c5f7f50168f9aa1cac4dbb7a89999eebb106
Mohamed Riyas. Ashraf Ali <riyassucces@gmail.com>
2024-02-02 19:32:34 +0530

    age calculate function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
 //select the input vlaues
 
 const input_day = document.querySelector("#day");
@@ -16,6 +19,10 @@ const submmit_button = document.querySelector(".button");
 const error_day = document.querySelector(".error-day");
 const error_month = document.querySelector(".error-month");
 const error_year = document.querySelector(".error-year");
+
+//submit button eventlistener
+submmit_button.addEventListener("click", calculate_age);
+
 //month event listener
 input_day.addEventListener("input", (e) => {
   // const inputValue = e.target.value;
@@ -58,18 +65,19 @@ input_month.addEventListener("input", (e) => {
     error_month.textContent = "";
   }
 });
+
 //year event listener
 input_year.addEventListener("input", (e) => {
   // const inputValue = e.target.value;
   // Perform actions based on the input value
   // console.log("Input value:", inputValue);
-  if (+input_year.value > 2024) {
+  if (+input_year.value > currentYear) {
     error_year.textContent = "this must be valid year";
     isvalid = false;
     return;
   } else {
     isvalid = true;
-    error_year.textContent = "2023";
+    error_year.textContent = "";
   }
   if (+input_year.value === 0) {
     isvalid = false;
@@ -79,3 +87,22 @@ input_year.addEventListener("input", (e) => {
     error_year.textContent = "";
   }
 });
+function calculate_age() {
+  if (isvalid) {
+    let birthday_date = `${input_month.value}/${input_day.value}/${input_year.value}`;
+    let birthday_date_obj = new Date(birthday_date);
+    let age_diff = Date.now() - birthday_date_obj.getTime();
+    let age_date = new Date(age_diff);
+
+    let age_years = age_date.getUTCFullYear() - 1970;
+    let age_months = age_date.getUTCMonth();
+    let age_days = age_date.getUTCDate() - 1;
+
+    // Update the corresponding DOM elements
+    output_day.textContent = age_days;
+    output_month.textContent = age_months;
+    output_year.textContent = age_years;
+  } else {
+    alert("Error in the form. Please check the inputs.");
+  }
+}
